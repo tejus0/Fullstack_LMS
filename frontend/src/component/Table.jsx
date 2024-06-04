da873e99b81b53a7ba3c8b45ce70b9ad715682b4
abhigyadufare <abhigyadufare@gmail.com>
2024-06-04 18:29:28 +0530

    student profile

--- frontend/src/component/Table.jsx
+++ frontend/src/component/Table.jsx
@@ -109,7 +109,7 @@ const Table = () => {
                                                     onChange={() => dispatch(handleSelectRow({ id: item._id }))}
                                                 />
                                             </td>
-                                            <td className="px-6 py-4 whitespace-nowrap"> <Link to={`/student/${item._id}`} state={{ from: `${item._id}` }}>{item.name}</Link></td>
+                                            <td className="px-6 py-4 whitespace-nowrap"> <Link to={`/student/${item._id}`} state={{ id:`${item._id}`}}>{item.name}</Link></td>
                                             <td className="px-6 py-4 whitespace-nowrap">{item.email}</td>
                                             <td className="px-6 py-4 whitespace-nowrap"> {item.contactNumber}</td>
                                             <td className="px-6 py-4 whitespace-nowrap">{item.state}</td>
--- frontend/src/pages/StudentProfile.jsx
+++ frontend/src/pages/StudentProfile.jsx
@@ -6,6 +6,7 @@ import { useLocation } from 'react-router-dom';
 const StudentProfile = () => {
   const baseUrl = import.meta.env.VITE_API
   const location = useLocation();
+  console.log(location.state);
 
     // TODO :- all this bunch of cluster will be removed and added to redux slice 
     const [studentData, setStudentData] = useState([])
@@ -15,7 +16,7 @@ const StudentProfile = () => {
             try {
                 const student = await axios.get(`${baseUrl}${location.pathname}`);
                 const fetchedData = student.data.data[0];
-
+                console.log(fetchedData);
                 setStudentData(fetchedData);
                 console.log(studentData);
             } catch (e) {
@@ -29,15 +30,15 @@ const StudentProfile = () => {
     <>
     <div className='flex w-100'>
       <div className='bg-blue-300 p-5 rounded-lg'>
-      <h1>Student Name: {studentData.name}</h1>
-      <h1>Contact Number: {studentData.contactNumber}</h1>
-      <h1>Email: {studentData.email}</h1>
-      <h1>Whatsapp Number: {studentData.whatsappNumber}</h1>
+        <h1>Student Name: {studentData.name}</h1>
+        <h1>Contact Number: {studentData.contactNumber}</h1>
+        <h1>Email: {studentData.email}</h1>
+        <h1>Whatsapp Number: {studentData.whatsappNumber}</h1>
       </div>
       <div>Student Details</div>
     </div>
       <div className='flex justify-center'>
-        <Followup />
+        <Followup id={studentData._id} />
       </div>
     </>
   )
